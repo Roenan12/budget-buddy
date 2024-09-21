7a90c3967711d3f81604a74919e076f69cc74cee
Roenan Halili <haliliroenan12@gmail.com>
2024-09-21 23:47:49 +0800

    refactor: utilize auth feature with context | improve switching between login and register form UI add name input field with user icon

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../contexts/AuthContext";
 import styles from "./LoginForm.module.css";
 import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
+  const { handleLogin, handleRegister, isAuthenticated } = useAuthContext();
   const [isSignUp, setIsSignUp] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
 
   function togglePasswordVisibility() {
     setShowPassword((prevShowPassword) => !prevShowPassword);
@@ -37,57 +51,127 @@ function LoginForm() {
     setErrors(errors);
   }
 
-  function toggleForm() {
-    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
-    setErrors([]);
-  }
-
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
+    if (!email || !password) return;
+
+    handleLogin({ email, password });
+
+    if (isSignUp) {
+      if (password !== confirmPassword) {
+        setErrors(["Passwords do not match"]);
+        return;
+      }
+      handleRegister(name, email, password);
+    } else {
+      handleLogin(email, password);
+    }
   }
 
   return (
-    <>
-      <form className={styles.form_container} onSubmit={handleSubmit}>
-        <div className={styles.logo_container}>
-          <img
-            className={styles.logo}
-            src="./budget-logo.png"
-            alt="Budget Buddy logo"
-          />
-        </div>
-        <div className={styles.title_container}>
-          <h5 className={styles.title}>
-            {isSignUp ? "Create an Account" : "Login to your Account"}
-          </h5>
-          <span className={styles.subtitle}>
-            {isSignUp
-              ? "Sign up to get started with our app."
-              : "Get started with our app, just create an account and enjoy your experience."}
-          </span>
-        </div>
+    <form className={styles.form_container} onSubmit={handleSubmit}>
+      <div className={styles.logo_container}>
+        <img
+          className={styles.logo}
+          src="./budget-logo.png"
+          alt="Budget Buddy logo"
+        />
+      </div>
+      <div className={styles.title_container}>
+        <h5 className={styles.title}>
+          {isSignUp ? "Create an Account" : "Login to your Account"}
+        </h5>
+        <span className={styles.subtitle}>
+          {isSignUp
+            ? "Sign up to get started with our app."
+            : "Get started with our app, just create an account and enjoy your experience."}
+        </span>
+      </div>
+      {isSignUp && (
         <div className={styles.input_container}>
-          <label className={styles.input_label} htmlFor="email_field">
-            Email
+          <label className={styles.input_label} htmlFor="name_field">
+            Name
           </label>
           <div className={styles.input_wrapper}>
-            <img
-              src="/mail-icon.svg"
-              className={styles.icon}
-              alt="Email Icon"
-            />
+            <img src="/user-icon.svg" className={styles.icon} alt="User Icon" />
             <input
-              placeholder="name@mail.com"
-              type="email"
+              placeholder="your name"
+              type="text"
               className={styles.input_field}
-              id="email_field"
-              aria-label="Email"
+              id="name_field"
+              aria-label="name_field"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
         </div>
+      )}
+      <div className={styles.input_container}>
+        <label className={styles.input_label} htmlFor="email_field">
+          Email
+        </label>
+        <div className={styles.input_wrapper}>
+          <img src="/mail-icon.svg" className={styles.icon} alt="Email Icon" />
+          <input
+            placeholder="name@mail.com"
+            type="email"
+            className={styles.input_field}
+            id="email_field"
+            aria-label="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+      </div>
+      <div className={styles.input_container}>
+        <label className={styles.input_label} htmlFor="password_field">
+          Password
+        </label>
+        <div className={styles.input_wrapper}>
+          <img
+            src="/password-icon.svg"
+            className={styles.icon}
+            alt="Password Icon"
+          />
+          <input
+            placeholder="password"
+            type={showPassword ? "text" : "password"}
+            className={styles.input_field}
+            id="password_field"
+            aria-label="Password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          {password && (
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={styles.toggle_btn}
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          )}
+        </div>
+        {isSignUp && errors.length > 0 && (
+          <div className={styles.error_tooltip}>
+            {errors.map((error, index) => (
+              <p key={index} className={styles.error_message}>
+                {error}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
+      {isSignUp && (
         <div className={styles.input_container}>
-          <label className={styles.input_label} htmlFor="password_field">
-            Password
+          <label
+            className={styles.input_label}
+            htmlFor="confirm_password_field"
+          >
+            Confirm Password
           </label>
           <div className={styles.input_wrapper}>
             <img
@@ -96,88 +180,48 @@ function LoginForm() {
               alt="Password Icon"
             />
             <input
-              placeholder="password"
+              placeholder="re-enter password"
               type={showPassword ? "text" : "password"}
               className={styles.input_field}
-              id="password_field"
-              aria-label="Password"
-              value={password}
-              onChange={handlePasswordChange}
+              id="confirm_password_field"
+              aria-label="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
             />
-            {password && (
-              <button
-                type="button"
-                onClick={togglePasswordVisibility}
-                className={styles.toggle_btn}
-              >
-                {showPassword ? "Hide" : "Show"}
-              </button>
-            )}
           </div>
-          {isSignUp && errors.length > 0 && (
-            <div className={styles.error_tooltip}>
-              {errors.map((error, index) => (
-                <p key={index} className={styles.error_message}>
-                  {error}
-                </p>
-              ))}
-            </div>
-          )}
         </div>
-        {isSignUp && (
-          <div className={styles.input_container}>
-            <label
-              className={styles.input_label}
-              htmlFor="confirm_password_field"
-            >
-              Confirm Password
-            </label>
-            <div className={styles.input_wrapper}>
-              <img
-                src="/password-icon.svg"
-                className={styles.icon}
-                alt="Password Icon"
-              />
-              <input
-                placeholder="re-enter password"
-                type={showPassword ? "text" : "password"}
-                className={styles.input_field}
-                id="confirm_password_field"
-                aria-label="Confirm Password"
-              />
-            </div>
-          </div>
-        )}
-        <Button type="login_btn">
-          <span>{isSignUp ? "Sign Up" : "Sign In"}</span>
-        </Button>
+      )}
 
-        <div className={styles.separator}>
-          <hr className={styles.line} />
-          <span>Or</span>
-          <hr className={styles.line} />
-        </div>
+      <Button type="login_btn">
+        <span>{isSignUp ? "Sign Up" : "Sign In"}</span>
+      </Button>
+      <div className={styles.separator}>
+        <hr className={styles.line} />
+        <span>Or</span>
+        <hr className={styles.line} />
+      </div>
 
-        <Button type="google_login">
-          <img src="/google-icon.svg" alt="Google Icon" />
-          <span>
-            {isSignUp ? "Sign Up with Google" : "Sign In with Google"}
-          </span>
-        </Button>
+      <Button type="google_login">
+        <img src="/google-icon.svg" alt="Google Icon" />
+        <span>{isSignUp ? "Sign Up with Google" : "Sign In with Google"}</span>
+      </Button>
 
-        <Button type="apple_login">
-          <img src="/apple-icon.svg" alt="Apple Icon" />
-          <span>{isSignUp ? "Sign Up with Apple" : "Sign In with Apple"}</span>
-        </Button>
+      <Button type="apple_login">
+        <img src="/apple-icon.svg" alt="Apple Icon" />
+        <span>{isSignUp ? "Sign Up with Apple" : "Sign In with Apple"}</span>
+      </Button>
 
-        <p>
-          {isSignUp ? "Already have an account?" : "Don't have an account?"}
-          <span className={styles.register} onClick={toggleForm}>
-            {isSignUp ? "Sign In" : "Sign Up"}
-          </span>
-        </p>
-      </form>
-    </>
+      <p>
+        {isSignUp ? "Already have an account?" : "Don't have an account?"}
+        <span
+          className={styles.register}
+          onClick={() => setIsSignUp(!isSignUp)}
+        >
+          {isSignUp ? "Sign In" : "Sign Up"}
+        </span>
+      </p>
+    </form>
   );
 }
 
