79e3e461c5adda4dba77dc9c698d4ec45003f796
Roenan Halili <haliliroenan12@gmail.com>
2024-09-27 22:45:41 +0800

    feat: implement logout functionality

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -2,3 +2,4 @@ export { default as dashboard } from "./dashboard-icon.svg";
 export { default as wallet } from "./wallet-icon.svg";
 export { default as savings } from "./savings-icon.svg";
 export { default as expenses } from "./expenses-icon.svg";
+export { default as logout } from "./logout-icon.svg";
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,61 +1,96 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./AppLayout.module.css";
-import { dashboard, wallet, savings, expenses } from "../assets/icons";
+import { dashboard, wallet, savings, expenses, logout } from "../assets/icons";
+import Button from "./Button";
+import { useAuthContext } from "../contexts/AuthContext";
+import Loader from "./Loader";
 
 function Sidebar() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const { handleLogout, isLoading } = useAuthContext();
+
+  async function handleClick() {
+    try {
+      await handleLogout();
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed: ", error.message);
+    }
+  }
+
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.logo}>
-        <img
-          className={styles.logoIcon}
-          src="./budget-icon.png"
-          alt="Budget Buddy Logo"
-        />
-        <h4 className={styles.logoName}>BUDGET BUDDY</h4>
+      <div>
+        <div className={styles.logo}>
+          <img
+            className={styles.logoIcon}
+            src="./budget-icon.png"
+            alt="Budget Buddy Logo"
+          />
+          <h4 className={styles.logoName}>BUDGET BUDDY</h4>
+        </div>
+        <nav>
+          <ul className={styles.navlinks}>
+            <li
+              onClick={() => navigate("/dashboard")}
+              className={
+                location.pathname === "/dashboard" ? styles.active : ""
+              }
+            >
+              <img
+                src={dashboard}
+                alt="Dashboard Icon"
+                className={styles.navIcon}
+              />
+              Dashboard
+            </li>
+            <li
+              onClick={() => navigate("/budgets")}
+              className={location.pathname === "/budgets" ? styles.active : ""}
+            >
+              <img src={wallet} alt="Wallet Icon" className={styles.navIcon} />
+              Budgets
+            </li>
+            <li
+              onClick={() => navigate("/savings")}
+              className={location.pathname === "/savings" ? styles.active : ""}
+            >
+              <img
+                src={savings}
+                alt="Savings Icon"
+                className={styles.navIcon}
+              />
+              Savings
+            </li>
+            <li
+              onClick={() => navigate("/expenses")}
+              className={location.pathname === "/expenses" ? styles.active : ""}
+            >
+              <img
+                src={expenses}
+                alt="Bill List Icon"
+                className={styles.navIcon}
+              />
+              Expenses
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul className={styles.navlinks}>
-          <li
-            onClick={() => navigate("/dashboard")}
-            className={location.pathname === "/dashboard" ? styles.active : ""}
-          >
-            <img
-              src={dashboard}
-              alt="Dashboard Icon"
-              className={styles.navIcon}
-            />
-            Dashboard
-          </li>
-          <li
-            onClick={() => navigate("/budgets")}
-            className={location.pathname === "/budgets" ? styles.active : ""}
-          >
-            <img src={wallet} alt="Wallet Icon" className={styles.navIcon} />
-            Budgets
-          </li>
-          <li
-            onClick={() => navigate("/savings")}
-            className={location.pathname === "/savings" ? styles.active : ""}
-          >
-            <img src={savings} alt="Savings Icon" className={styles.navIcon} />
-            Savings
-          </li>
-          <li
-            onClick={() => navigate("/expenses")}
-            className={location.pathname === "/expenses" ? styles.active : ""}
-          >
-            <img
-              src={expenses}
-              alt="Bill List Icon"
-              className={styles.navIcon}
-            />
-            Expenses
-          </li>
-        </ul>
-      </nav>
+
+      <div className={styles.bottom_container}>
+        <Button type="logout" onClick={handleClick} disabled={isLoading}>
+          {isLoading ? (
+            <Loader color="black" />
+          ) : (
+            <>
+              <img src={logout} className={styles.navIcon} />
+              Logout
+            </>
+          )}
+        </Button>
+      </div>
     </aside>
   );
 }
