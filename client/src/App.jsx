ce92a5baeeafcfaed2ae292be57aa804bedb009f
Roenan Halili <haliliroenan12@gmail.com>
2024-09-27 22:47:24 +0800

    feat: add protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import {
 } from "./pages";
 import { AuthProvider } from "./contexts/AuthContext";
 import AppLayout from "./components/AppLayout";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -21,7 +22,13 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
 
-            <Route element={<AppLayout />}>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/budgets" element={<Budgets />} />
               <Route path="/savings" element={<Savings />} />
